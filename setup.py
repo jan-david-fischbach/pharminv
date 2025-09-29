e33e2a072de3cb1476b47d0439963018e54977d6
Jan David Fischbach <mail@jan-dav.id>
2025-09-29 22:07:06 +0200

    make compatible to modern python

--- setup.py
+++ setup.py
@@ -28,25 +28,17 @@ except ImportError:
 
 extensions = [Extension('harminv.charminv',
                         ["harminv/charminv" + ext],
-                        include_dirs=[numpy.get_include()],
+                        include_dirs=[numpy.get_include(), "."],
                         libraries=['harminv'],
                         )]
 
 if USE_CYTHON:
-    extensions = cythonize(extensions)
+    extensions = cythonize(extensions, include_path=["harminv"])
     from Cython.Distutils import build_ext
     cmdclass.update(build_ext=build_ext)
 
 setup(
-    name='pharminv',
-    version="0.4",
-    description='Python interface to harminv',
-    long_description=readme,
     packages=['harminv'],
-    author="Aaron O'Leary",
-    author_email='dev@aaren.me',
-    license='GPLv3',
-    url='http://github.com/aaren/pharminv',
     cmdclass=cmdclass,
     ext_modules=extensions
 )
